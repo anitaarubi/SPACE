8a71716ecb1e240c8c4d421cc41a5312fbcf7e49
anitaarubi <anitaarubi9@gmail.com>
2024-01-05 15:03:30 +0100

    Added main.py; this program calls the automation.py file

---
+++ main.py
@@ -0,0 +1,19 @@
+# Name: main.py
+# Course: General Business Administration & Accounting
+# Author: Ewo-oritse Anita Arubi
+# Date: 29th December 2023
+
+
+# import useful native libraries
+import time
+
+# import external libraries
+from geopy.geocoders import Nominatim
+
+# import function from file
+from automation import go
+
+# run the main program
+if __name__ == "__main__":
+    print(go())
+    time.sleep(3)
